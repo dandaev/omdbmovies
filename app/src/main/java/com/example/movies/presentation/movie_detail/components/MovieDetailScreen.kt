0aec9e64fa1ecac2b899a84d222de5c5f9ad1330
Alybek <dandaev.alybek@gmail.com>
2023-06-01 01:39:34 +0200

    Backdrop feature in movie detail screen realized (next - fix bugs and make style prettier)

--- app/src/main/java/com/example/movies/presentation/movie_detail/components/MovieDetailScreen.kt
+++ app/src/main/java/com/example/movies/presentation/movie_detail/components/MovieDetailScreen.kt
@@ -1,37 +1,109 @@
 package com.example.movies.presentation.movie_detail
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.movies.R
+import com.example.movies.domain.model.MovieDetail
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MovieDetailScreen(
     state: MovieDetailState
 ) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        state.movie?.let { movie ->
-            LazyColumn(
-                modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(20.dp)
+    val scaffoldState = rememberBackdropScaffoldState(BackdropValue.Revealed)
+    val scrollState = rememberScrollState()
+    val scope = rememberCoroutineScope()
+    BackdropScaffold(
+        scaffoldState = scaffoldState,
+        gesturesEnabled = false,
+        appBar = { },
+        backLayerContent = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+                    .background(Color.Blue)
             ) {
-                item {
-                    Row(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = movie.title,
-                            style = MaterialTheme.typography.h2,
-                            modifier = Modifier.weight(8f)
-                        )
-                    }
-                }
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(state.movie?.poster)
+                        .crossfade(true)
+                        .build(),
+                    placeholder = painterResource(R.drawable.movie_default),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = Color.Black)
+                )
+            }
+        },
+        frontLayerElevation = 30.dp,
+        frontLayerShape = MaterialTheme.shapes.large,
+        frontLayerContent = {
+            state.movie?.let { movie ->
+                FrontLayerScreen(movie)
+            }
+
+        }
+    ){
+        if (scrollState.isScrollInProgress){
+            scope.launch {
+                scaffoldState.conceal()
             }
         }
     }
-
 }
+
+
+@Composable
+fun FrontLayerScreen(
+    movie: MovieDetail
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            item {
+                Box(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        text = movie.title + "(${movie.year})",
+                        style = MaterialTheme.typography.h4,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    text = movie.plot,
+                    style = MaterialTheme.typography.body2
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    text = "Actors",
+                    style = MaterialTheme.typography.h5
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    text = movie.actors,
+                    style = MaterialTheme.typography.body1
+                )
+            }
+        }
+    }
+}
+
+
--- app/src/main/java/com/example/movies/presentation/movie_search/components/MovieSearchViewHolder.kt
+++ app/src/main/java/com/example/movies/presentation/movie_search/components/MovieSearchViewHolder.kt
@@ -1,8 +1,10 @@
 package com.example.movies.presentation.movie_search.components
 
 import android.content.Context
+import androidx.compose.ui.res.painterResource
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import coil.compose.AsyncImage
 import com.example.movies.R
 import com.example.movies.databinding.SearchItemMovieBinding
 import com.example.movies.domain.model.Movie
@@ -19,6 +21,9 @@ class MovieSearchViewHolder(
         Picasso.get().load(movie.poster).placeholder(ContextCompat.getDrawable(context,
             R.drawable.movie_default
         )!!).error(ContextCompat.getDrawable(context, R.drawable.movie_default)!!).into(searchItemMovieBinding.movieLogo)
+//        searchItemMovieBinding.movieLogo.setContent {TODO Realize it with Coil
+//            AsyncImage(model = movie.poster, placeholder = painterResource(id = R.drawable.movie_default), contentDescription = null)
+//        }
         searchItemMovieBinding.title.text = movie.title
         searchItemMovieBinding.year.text = movie.year
         searchItemMovieBinding.type.text = movie.type
